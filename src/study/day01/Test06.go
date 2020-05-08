d399c6ee9292059dfbae745b0a26345e417c8023
jiangjx <640364157@qq.com>
2020-05-08 13:32:43 +0800

    初始化

--- src/study/day01/Test06.go
+++ src/study/day01/Test06.go
@@ -1,4 +1,4 @@
-//流程结构(if)
+//流程结构
 package main
 
 import (
@@ -36,6 +36,93 @@ func func12() {
 	}
 }
 
+//switch
+func func13() {
+	var a int
+	fmt.Println("请输入你的语文成绩")
+	fmt.Scanln(&a)
+	//注:go的switch不需要break来跳出,相反,如果需要贯通,需要fallthrough关键字
+	switch a {
+	case 90:
+		fmt.Println("你的成绩为: 优秀")
+	case 80:
+		fmt.Println("你的成绩为: 良好")
+	case 60, 70:
+		fmt.Println("你的成绩为: 及格")
+	default:
+		fmt.Println("你的成绩为: 不及格")
+	}
+}
+
+//type-switch
+func func14(x interface{}) {
+	switch t := x.(type) {
+	case nil:
+		fmt.Printf("X的类型: %T", t)
+	case string:
+		fmt.Printf("X的类型: %s", "string")
+	case bool:
+		fmt.Printf("X的类型: %s", "bool")
+	case int:
+		fmt.Printf("X的类型: %s", "int")
+	case uint:
+		fmt.Printf("X的类型: %s", "uint")
+	case float32, float64:
+		fmt.Printf("X的类型: %s", "float")
+	default:
+		fmt.Printf("X的类型: %s", "非基本类型")
+	}
+}
+
+//循环语句(for)
+func func15() {
+	//基本循环
+	for i := 0; i < 10; i++ {
+		fmt.Print(i)
+	}
+	fmt.Println()
+	//range迭代(range可以迭代:切片,列表, 元组, 字典等集合结构)
+	a := "0123456789"
+	for i := range a {
+		fmt.Print(i)
+	}
+	fmt.Println()
+	//break跳出循环
+	for i := range a {
+		if i == 5 {
+			break
+		}
+		fmt.Print(i)
+	}
+	fmt.Println()
+	//continue进入下一次循环
+	for i := range a {
+		if i == 5 {
+			continue
+		}
+		fmt.Print(i)
+	}
+}
+
+//goto语句
+func func16() {
+	a := 10
+
+LOOP:
+	for a < 20 {
+		if a == 15 {
+			a++
+			goto LOOP
+		}
+		fmt.Println(a)
+		a++
+	}
+}
+
 func main() {
-	func12()
+	//func12()
+	//func13()
+	//func14('1')
+	//func15()
+	func16()
 }
--- src/study/day01/Test07.go
+++ src/study/day01/Test07.go
@@ -1,25 +1,73 @@
+//集合结构-数组
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-//switch
-func func13() {
-	var a int
-	fmt.Println("请输入你的语文成绩")
-	fmt.Scanln(&a)
-	//注:go的switch不需要break来跳出,相反,如果需要贯通,需要fallthrough关键字
-	switch a {
-	case 90:
-		fmt.Println("你的成绩为: 优秀")
-	case 80:
-		fmt.Println("你的成绩为: 良好")
-	case 60, 70:
-		fmt.Println("你的成绩为: 及格")
-	default:
-		fmt.Println("你的成绩为: 不及格")
+//数组
+func func17() {
+	//定义数组会给默认值
+	var a [10]int
+	fmt.Println(a)
+	//定义数组,元素个数不能大于数组长度
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(b)
+	//定义数组,自动匹配元素个数
+	c := []string{"a", "b", "c", "d", "e", "f"}
+	fmt.Println(c)
+	//数组的遍历1
+	for i := 0; i < len(c); i++ {
+		fmt.Println(c[i])
+	}
+	//数组的遍历2
+	for x, y := range c {
+		fmt.Println(x, y)
 	}
 }
 
+//多维数组
+func func18() {
+	a := [3][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}
+	fmt.Println(a)
+	//遍历1
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a[i]); j++ {
+			fmt.Print(a[i][j], " ")
+		}
+		fmt.Println()
+	}
+	//遍历2
+	for _, arr := range a {
+		for _, v := range arr {
+			fmt.Print(v, " ")
+		}
+		fmt.Println()
+	}
+}
+
+//数组内是值类型,而不是引用,所以数组a赋值给一个新的变量b,b元素改变不会影响a
+func func19() {
+	a := []int{1, 2, 3}
+	b := a
+	b = append(b, 4)
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+//数组的排序(冒泡排序，插入排序，选择排序，希尔排序，堆排序，快速排序)
+func func20() {
+	arr := []int{4, 3, 5, 1, 2}
+	fmt.Println(arr)
+	//默认使用的快速排序
+	sort.Ints(arr)
+	fmt.Println(arr)
+}
+
 func main() {
-	func13()
+	//func17()
+	//func18()
+	//func19()
+	func20()
 }
