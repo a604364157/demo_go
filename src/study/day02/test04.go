ed9ea3dd63ba627e2b2237b2e7cc4d997b65b4c6
Fzgj <1209320219@qq.com>
2020-05-13 22:45:53 +0800

    初始化

--- src/study/day02/test04.go
+++ src/study/day02/test04.go
@@ -1,6 +1,26 @@
 //接口
 package main
 
-func main() {
+import "fmt"
 
+//接口的定义
+type Phone interface {
+	call()
+}
+
+type IPhone struct {
+}
+
+func (IPhone) call() {
+	fmt.Println("iphone call")
+}
+
+func func26() {
+	var phone Phone
+	phone = new(IPhone)
+	phone.call()
+}
+
+func main() {
+	func26()
 }
