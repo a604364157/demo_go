69ae7a6e7bd2ac8ec7d716367ea9a200a98e6b86
jiangjx <640364157@qq.com>
2020-05-22 09:43:17 +0800

    初始化

---
+++ src/study/day04/test01.go
@@ -0,0 +1,13 @@
+//并发编程
+/*并发编程，什么是并行，并发，串行。Go语言如何实现并发编程
+goroutine的使用。runtime包、sync包的介绍。channel通道的使用，
+以及缓冲通道，定向通道。select语句，time包中和并发编程相关的函数介绍*/
+package main
+
+func func01() {
+
+}
+
+func main() {
+
+}
--- src/study/hello.go
+++ src/study/hello.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println("github/TheAlgorithms")
 	fmt.Println("kotlin学习地址 \thttps://www.cnblogs.com/Jetictors/p/9227498.html\n\ngolang学习地址\thttps://www.qfgolang.com/\n\npython学习地址\thttp://c.biancheng.net/python/")
 }
